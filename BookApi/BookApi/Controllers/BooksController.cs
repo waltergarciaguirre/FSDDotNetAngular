899683160903397ac51979f4c88b812a1d4ef691
Walter Garciaguirre <waltergarciaguirre@gmail.com>
2025-06-07 06:59:02 -0500

    first controller and Model Created

---
+++ BookApi/BookApi/Controllers/BooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    //https://localhost:7082/api/books
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+    }
+}
